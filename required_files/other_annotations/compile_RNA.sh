09d8947f9135e4ae30d42ee9e5a1c8e2342a7702
nicole.lake@yale.edu <nicole.lake@yale.edu>
2022-12-06 17:46:35 -0500

    adding input files

--- required_files/other_annotations/compile_RNA.sh
+++
@@ -1,9 +0,0 @@
-# generate a single file to be used to determine base type in RNA
-head -1 /Users/nicole.lake/Dropbox/Yale/PROJECTS/MitoVisualize/2019_Summer_Student_Project/github_mito_SVGs/tsvs_final/MT-TA_with_pairs.tsv | tr -d '\r' | awk 'BEGIN{OFS="\t"} {print $0,"file"}' > required_files/other_annotations/all_RNA_bases.tsv
-
-for file in /Users/nicole.lake/Dropbox/Yale/PROJECTS/MitoVisualize/2019_Summer_Student_Project/github_mito_SVGs/tsvs_final/*tsv;
-do
-	name=${file#*final/}
-	echo $name
-	tail -n +2 -q $file | tr -d '\r' | awk -v n=$name 'BEGIN{OFS="\t"} {print $0,n}' >> required_files/other_annotations/all_RNA_bases.tsv
-done
